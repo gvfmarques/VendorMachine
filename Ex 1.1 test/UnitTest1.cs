d1a946d64faef0a36db7ff1bb87c2f08ca3d7a49
Glauber Marques <glauberfmarques@gmail.com>
2021-01-28 21:59:27 -0600

    Unit test Implementation in Exercise 01

---
+++ Ex 1.1 test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ex_1._1_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
