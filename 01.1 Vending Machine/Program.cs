c23f7e040031a53d2caf41ee113988f485fce842
Glauber Marques <glauberfmarques@gmail.com>
2021-01-28 21:40:13 -0600

    Add project files.

---
+++ 01.1 Vending Machine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._1_Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 01
+            // Author: Marques, Glauber
+
+            Console.WriteLine("Welcome to the .NET C# Soda Vending Machine ");
+            Console.Write("Please insert 35 cents: ");
+            int valueInserted = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have inserted {0} cents", valueInserted);
+            Console.WriteLine("Thanks. Here is your soda.");
+        }
+    }
+}
